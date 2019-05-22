250e9671bfedeafb6071407d7b786d1ce2db1c7b
kaprince <kaprince@sheridancollege.ca>
2019-05-22 09:42:05 -0400

    initial commit

---
+++ src/studentweek3/Studentweek3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentweek3;
+
+/**
+ *
+ * @author Prince
+ */
+public class Studentweek3 {
+     
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student s1 = new Student();
+        s1.setFirstName("John");
+        s1.setID(4);
+    }
+    
+}
