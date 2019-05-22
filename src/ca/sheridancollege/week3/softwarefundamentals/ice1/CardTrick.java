03348fe273884c9480795cc65ed0d115d53ca5c3
kaprince <50666683+kaprince@users.noreply.github.com>
2019-05-22 11:54:12 -0400

    Update CardTrick.java
     “ Hard-coded luckyCard”

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -28,7 +28,7 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            Card luckycard = new Card(3, Hearts);
+            Card luckycard = new Card(4, Hearts);
             
             Random rand = new Random();
             int value = rand.nextInt((13 - 1) + 1) + 1;
